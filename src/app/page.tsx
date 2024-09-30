1d72cee2edc1be1a372bfbe6b5d3249d92a872ae
Alirezaw <Alirezawmoradi@gmail.com>
2024-09-30 21:14:38 +0330

    sticky note component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import {StickyNote} from "@/app/_components/sticky-note/sticky-note";
+
 export default function Home() {
     return (
-        <div className='flex justify-center items-center min-h-screen font-extrabold text-5xl'>
-            <h1> Sticky Note Application</h1>
-        </div>
-    );
+        <>
+            <StickyNote/>
+        </>
+    )
 }
