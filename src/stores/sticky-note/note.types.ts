ba3ad5331e541eb3450c3f4a9a364bf484709e0c
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-01 16:05:27 +0330

    note store types created

---
+++ src/stores/sticky-note/note.types.ts
@@ -0,0 +1,17 @@
+export type NotesState = {
+    notes: Note[];
+    actions: {
+        addNote: (text: string) => void;
+        deleteNote: (id: number) => void;
+        updateNote: (id: number, newText: string) => void;
+        toggleEditing: (id: number, isEditing: boolean) => void;
+        changeColor: (id: number, color: string) => void;
+    }
+};
+
+type Note = {
+    id: number;
+    text: string;
+    isEditing: boolean;
+    color: string;
+};
