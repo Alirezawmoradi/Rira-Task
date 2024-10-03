781a283c3eaf471a65e8d5db37272c5b0cc49405
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-03 14:54:40 +0330

    created date property added to note

--- src/stores/sticky-note/useNoteStore.ts
+++ src/stores/sticky-note/useNoteStore.ts
@@ -4,9 +4,9 @@ import {colors} from "@/app/utils/colors";
 
 export const useNoteStore = create<NotesState>((set) => ({
     notes: [
-        {id: 1, text: "test", isEditing: false, color: colors[0]},
-        {id: 2, text: "test", isEditing: false, color: colors[1]},
-        {id: 3, text: "test", isEditing: false, color: colors[2]}
+        {id: 1, text: "test", isEditing: false, color: colors[0], createdAt: new Date()},
+        {id: 2, text: "test", isEditing: false, color: colors[1], createdAt: new Date()},
+        {id: 3, text: "test", isEditing: false, color: colors[2], createdAt: new Date()}
     ],
 
     actions: {
@@ -14,7 +14,13 @@ export const useNoteStore = create<NotesState>((set) => ({
         addNote: (text: string) => {
             const randomColor = colors[Math.floor(Math.random() * colors.length)];
             set((state) => ({
-                notes: [...state.notes, {id: state.notes.length + 1, text, isEditing: true, color: randomColor}],
+                notes: [...state.notes, {
+                    id: state.notes.length + 1,
+                    text,
+                    isEditing: true,
+                    color: randomColor,
+                    createdAt: new Date()
+                }],
             }))
         },
 
@@ -47,7 +53,7 @@ export const useNoteStore = create<NotesState>((set) => ({
                 const [draggedNote] = updatedNotes.splice(draggedNoteIndex, 1);
                 updatedNotes.splice(targetNoteIndex, 0, draggedNote);
 
-                return { notes: updatedNotes };
+                return {notes: updatedNotes};
             }),
 
     }
