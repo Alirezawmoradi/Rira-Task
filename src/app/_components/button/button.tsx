e3dc53eb60d5d2dd82928f9570f75a4ff7f9aaca
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-04 02:39:45 +0330

    addButton component refactored

--- src/app/_components/button/button.tsx
+++ src/app/_components/button/button.tsx
@@ -1,14 +1,21 @@
-export const Button: React.FC<ButtonProps> = ({
+import {BiPlus} from "react-icons/bi";
+import React from "react";
+
+export const AddButton: React.FC<ButtonProps> = ({
                                                   buttonText,
                                                   onClick,
                                                   className = '',
                                               }) => {
     return (
-        <button
-            className={`bg-blue-500 text-white py-2 px-4 rounded-md shadow hover:bg-blue-400 transition-all ${className}`}
-            onClick={onClick}
-        >
-            {buttonText}
-        </button>
+        <div
+            className={`flex flex-col w-5/6 h-64 p-6 relative bg-purple-700/50 hover:bg-purple-500/50 m-4 text-center shadow-lg transform rounded-xl transition-all duration-500 sliding ${className}`}>
+            <p className='text-sm text-gray-700 mt-2 font-bold'>{buttonText}</p>
+            <div className='flex flex-col h-full items-center justify-center text-gray-800'>
+                <BiPlus
+                    className='font-extrabold rounded-full p-1 cursor-pointer bg-gray-700/70 hover:bg-gray-500 transition-all duration-500 text-gray-200 text-3xl animate-bounce'
+                    onClick={onClick}
+                />
+            </div>
+        </div>
     );
 };
